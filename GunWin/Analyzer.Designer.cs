d9f60c4cea1abd2ebae8ee57f573389c7d2f2e1e
Thaddeus Ryker <thad@edgerunner.org>
2020-03-02 00:59:47 -0500

    Basic tweaks to gui app.

--- GunWin/Analyzer.Designer.cs
+++ GunWin/Analyzer.Designer.cs
@@ -28,13 +28,37 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
+         this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+         this.splitContainer1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // splitContainer1
+         // 
+         this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+         this.splitContainer1.Name = "splitContainer1";
+         this.splitContainer1.Size = new System.Drawing.Size(969, 633);
+         this.splitContainer1.SplitterDistance = 323;
+         this.splitContainer1.TabIndex = 0;
+         // 
+         // Analyzer
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(800, 450);
-         this.Text = "Form1";
+         this.ClientSize = new System.Drawing.Size(969, 633);
+         this.Controls.Add(this.splitContainer1);
+         this.Name = "Analyzer";
+         this.Text = "Analyzer";
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+         this.splitContainer1.ResumeLayout(false);
+         this.ResumeLayout(false);
+
       }
 
       #endregion
+
+      private System.Windows.Forms.SplitContainer splitContainer1;
    }
 }
 
